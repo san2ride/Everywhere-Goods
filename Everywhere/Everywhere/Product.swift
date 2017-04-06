7d37f45adafb93793fbdf8f417554f15290d2373
san2ride <sanchezjason@me.com>
2017-04-06 00:04:50 -0600

    Round1: tabBarController

---
+++ Everywhere/Everywhere/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  Everywhere
+//
+//  Created by don't touch me on 4/5/17.
+//  Copyright © 2017 trvl, LLC. All rights reserved.
+//
+
+import Foundation
---
+++ Everywhere/Everywhere/ProductCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCollectionViewCell.swift
+//  Everywhere
+//
+//  Created by don't touch me on 4/5/17.
+//  Copyright © 2017 trvl, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+}
